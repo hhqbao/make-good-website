54c40273bd0f2214b2b8b46bf0b1c53e847699e4
Bao Ho <hhqbao@gmail.com>
2025-07-10 14:19:48 +1000

    Initialize project files

---
+++ makegood.client/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
