234bd9532b3c1de2aa1340b12b63b914d143c00b
Bao Ho <hhqbao@gmail.com>
2025-07-29 11:51:25 +1000

    Clearing website up.

--- makegood.client/src/app/app.module.ts
+++ makegood.client/src/app/app.module.ts
@@ -6,13 +6,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainBannerComponent } from './components/main-banner/main-banner.component';
-import { ServicesCardComponent } from './components/services-card/services-card.component';
-import { ServicesBannerComponent } from './components/services-banner/services-banner.component';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
-import { SloganPanelComponent } from './components/slogan-panel/slogan-panel.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { MouldSectionComponent } from './components/mould-section/mould-section.component';
 import { MakeGoodComponent } from './components/make-good/make-good.component';
 import { ContactSectionComponent } from './components/contact-section/contact-section.component';
 
@@ -21,12 +16,9 @@ import { ContactSectionComponent } from './components/contact-section/contact-se
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MainBannerComponent,
-    ServicesCardComponent,
-    ServicesBannerComponent,
+
     TopNavComponent,
-    SloganPanelComponent,
-    MouldSectionComponent,
+
     FooterComponent,
     ContactSectionComponent,
     MakeGoodComponent,
